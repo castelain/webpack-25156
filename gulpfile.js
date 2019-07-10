346feec6e4d273415284d6b8687b227312321320
castelain <2804755011@qq.com>
2019-07-10 19:39:52 +0800

    add webpack-stream

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
+const webpack = require('webpack-stream');
 
 gulp.task('webpack', (done) => {
-
+    gulp.src('./src/*')
+        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(gulp.dest('./dist/'));
     done();
 });
