b14992a15f14c421c8209c5d0b6d20650aa19243
castelain <2804755011@qq.com>
2019-07-10 20:23:45 +0800

    config gulp-minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
+const minify = require('gulp-minify');
 
 gulp.task('webpack', (done) => {
     gulp.src('./src/*')
+        .pipe(minify())
         .pipe(webpack(require('./webpack.config.js')))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest('./dist'));
     done();
 });
